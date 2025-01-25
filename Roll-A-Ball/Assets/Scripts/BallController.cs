94ed31122cdfad27380d52a9cdb19142c553f125
Harper Kerstens <63266571+harperkerstens@users.noreply.github.com>
2025-01-24 17:12:41 -0800

    Update BallController.cs
    Updated BallController.cs

--- Roll-A-Ball/Assets/Scripts/BallController.cs
+++ Roll-A-Ball/Assets/Scripts/BallController.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Rigidbody sphereRigidbody;
+    public float ballSpeed = 2f; 
     void Start()
     {
         
@@ -11,6 +12,29 @@ public class BallController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 inputVector = Vector2.zero;
+        if(Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("User's Input is: W");
+            inputVector += Vector2.up;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("User's Input is: S");
+            inputVector += Vector2.down;
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("User's Input is: A");
+            inputVector += Vector2.left;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("User's Input is: D");
+            inputVector += Vector2.right;
+        }
+
+        Vector3 inputXZPlane = new Vector3(inputVector.x, 0, inputVector.y);
+        sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
     }
 }
